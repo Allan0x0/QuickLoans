3a6b98eb14ce8d4afa9e2fcdcbcb1dfb504cfb0e
Allan <allansimoyi@gmail.com>
2026-03-19 20:53:16 +0200

    append

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -10,7 +10,7 @@ module.exports = {
       env: {
         NODE_ENV: 'production',
         PORT: 3004,
-        DATABASE_URL: 'mysql://allan:function6781@localhost:3306/loans',
+        DATABASE_URL: 'mysql://allan:Function%406781@localhost:3306/loans',
         SESSION_SECRET: '78bee6e78ca663f1b4c618c5e9928dd6',
         CLOUDINARY_API_KEY: '993114726362865',
         CLOUDINARY_API_SECRET: 'Rp2KAu3wG3ydmJrQQmxp-iZM-LY',
